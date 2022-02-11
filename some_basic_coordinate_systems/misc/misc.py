082229b40009c0a023a44e948b43e510d76cf6a9
Noonuu <astrophysicsandpython@gmail.com>
2022-02-12 00:54:00 +0500

    import changes in misc.py

--- some_basic_coordinate_systems/misc/misc.py
+++ some_basic_coordinate_systems/misc/misc.py
@@ -1,41 +1,37 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Thu Apr  1 01:31:56 2021
-
-@author: noonu
 """
 
-import errors.error_base as eb
-import errors.error_messages as em
+from ..errors.error_base import EmptyList
+from ..errors.error_messages import AssumeX, AssumeXY
 
 
 def check_list_len_2d(list_to_check):
     try:
         _list_to_check = list(list_to_check)
         if len(_list_to_check) == 0:
-            raise eb.EmptyList
+            raise EmptyList
         elif len(_list_to_check) == 1:
-            em.AssumeX()
+            AssumeX()
             _list_to_check.extend([0, 0])
 
         return _list_to_check
-    except eb.EmptyList:
-        em.EmptyList(2)
+    except EmptyList:
+        EmptyList(2)
 
 
 def check_list_len_3d(list_to_check):
     try:
         _list_to_check = list(list_to_check)
         if len(_list_to_check) == 0:
-            raise eb.EmptyList
+            raise EmptyList
         elif len(_list_to_check) == 1:
-            em.AssumeX()
+            AssumeX()
             _list_to_check.extend([0, 0])
         elif len(_list_to_check) == 2:
-            em.AssumeXY()
+            AssumeXY()
             _list_to_check.append(0)
 
         return _list_to_check
-    except eb.EmptyList:
-        em.EmptyList(3)
+    except EmptyList:
+        EmptyList(3)
