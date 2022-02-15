fd27a0c35e79515ee9a021fdf06c9611e30265f5
Noonuu <astrophysicsandpython@gmail.com>
2022-02-16 01:24:42 +0500

    Changed distance_formula_spherical to distance_formula, changed translation_in_spherical_coordinates to translation_in_coordinates, and some minor updates
    Signed-off-by: Noonuu <astrophysicsandpython@gmail.com>

--- some_basic_coordinate_systems/spherical_coordinates.py
+++ some_basic_coordinate_systems/spherical_coordinates.py
@@ -11,7 +11,7 @@ import utilities
 TFloats = Tuple[float, float, float]
 
 
-def distance_formula_spherical(initial_coordinates: TFloats, final_coordinates: TFloats = None, deg_rad: str = 'rad') -> float:
+def distance_formula(initial_coordinates: TFloats, final_coordinates: TFloats = None, deg_rad: str = 'rad') -> float:
     """
     Calculates the distance between two given points in spherical coordinate system
 
@@ -35,14 +35,10 @@ def distance_formula_spherical(initial_coordinates: TFloats, final_coordinates: 
 
     theta1, phi1, theta2, phi2 = np.radian([theta1, phi1, theta2, phi2]) if deg_rad == 'deg' else [theta2, phi1, theta2, phi2]
 
-    _frac1 = pow(r1, 2) + pow(r2, 2)
-    _frac2 = - 2 * r1 * r2 * (np.sin(theta1) * np.sin(theta2) * np.cos(phi1 - phi2))
-    _frac3 = np.cos(theta1) * np.cos(theta2)
-
-    return pow(_frac1 + _frac2 + _frac3, 0.5)
+    return np.sqrt(r1**2 + r2**2 - 2 * r1 * r2 * (np.sin(theta1) * np.sin(theta2) * np.cos(phi1 - phi2) + np.cos(theta1) * np.cos(theta2)))
 
 
-def translation_in_spherical_coordinates(change_in_coordinates: TFloats, starting_point: TFloats = (0, 0, 0), deg_rad: str = 'rad'):
+def translation_in_coordinates(change_in_coordinates: TFloats, starting_point: TFloats = (0, 0, 0), deg_rad: str = 'rad'):
     """
     Calculates new coordinates of the point in spherical system given the translation in the object's coordinates.
 
