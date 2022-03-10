02c4bfd4d83548fc521722c7401943a6fbb6cb49
noonu <astrophysicsandpython@gmail.com>
2022-03-10 09:46:40 +0500

    conversion change
    * major changes in cartesian_spherical.py
    
    Signed-off-by: noonu <astrophysicsandpython@gmail.com>

--- some_basic_coordinate_systems/conversions/cartesian_spherical.py
+++ some_basic_coordinate_systems/conversions/cartesian_spherical.py
@@ -2,52 +2,71 @@
 Created on Thu Apr  1 01:12:40 2021
 """
 
+from typing import Tuple
+
 import numpy as np
 
 
-def coordinate_balance(x, y, value):
-    if x > 0:
-        if y > 0:
-            value = value
-        elif y < 0:
-            value = 7 * value
-    elif x < 0:
-        if y > 0:
-            value = 3 * value
-        elif y < 0:
-            value = 5 * value
-
-    return np.abs(value)
-
-
-def cartesian_spherical_converter(coordinates_3d, convert_to=None, deg_rad=None):
+def spherical_to_cartesian(spherical_coordinates: Tuple[float, float, float], deg_rad: str = 'rad'):
     """
-    Converts Cartesian <=> Spherical coordinate system
+    Converts spherical coordinates to Cartesian/rectangular coordinates.
 
-    :param coordinates_3d: coordinates of the point in spherical or Cartesian system
-    :param convert_to: string specifying either spherical/cartesian conversion
-    :param deg_rad: whether the theta, phi parameters of spherical coordinates are in deg/rad units.
-    :return: coordinates in covert_to coordinate system
+    Parameters
+    ----------
+    spherical_coordinates : Tuple[float, float, float]
+        A tuple containing spherical coordinates.
+    deg_rad : str, optional
+        Indication whether the input coordinates are in degrees or radians. The default is 'rad'.
+
+    Returns
+    -------
+    x : float
+        x-coordinate of spherical coordinates.
+    y : float
+        y-coordinate in spherical coordinates.
+    z : float
+        z-coordinate in spherical coordinates.
+
     """
-    if convert_to.lower() == 'spherical':
-        # https://opentextbc.ca/calculusv3openstax/chapter/cylindrical-and-spherical-coordinates/
-        x, y, z = coordinates_3d
-        out1 = pow(pow(x, 2) + pow(y, 2) + pow(z, 2), 0.5)
-        out2 = coordinate_balance(x, y, np.arctan(y / x))
-        _num, _den = np.sqrt(pow(x, 2) + pow(y, 2)), z
-        out3 = np.arctan(_num / _den)
-    elif convert_to.lower() == 'cartesian':
-        # https://tutorial.math.lamar.edu/classes/calciii/SphericalCoords.aspx
-        rho, theta, phi = coordinates_3d
-        if deg_rad == 'deg':
-            theta, phi = np.radians(theta), np.radians(phi)
-        out1 = rho * np.cos(theta) * np.sin(phi)
-        out2 = rho * np.sin(theta) * np.sin(phi)
-        out3 = rho * np.cos(phi)
-    else:
-        print('Please specify convert_to to either spherical or cartesian.')
-        return None, None, None
 
-    out1, out2, out3 = round(out1, 4), round(out2, 4), round(out3, 4)
+    cs_ = np.deg2rad(spherical_coordinates) if deg_rad == 'deg' else spherical_coordinates
 
-    return out1, out2, out3
+    rho, theta, phi = cs_
+
+    x = rho * np.sin(phi) * np.cos(theta)
+    y = rho * np.sin(phi) * np.sin(theta)
+    z = rho * np.cos(phi)
+
+    return x, y, z
+
+
+def cartesian_to_spherical(rectangular_coordinates, deg_rad='rad'):
+    """
+
+
+    Parameters
+    ----------
+    rectangular_coordinates : TYPE
+        DESCRIPTION.
+    deg_rad : TYPE, optional
+        DESCRIPTION. The default is 'rad'.
+
+    Returns
+    -------
+    rho : TYPE
+        DESCRIPTION.
+    theta : TYPE
+        DESCRIPTION.
+    phi : TYPE
+        DESCRIPTION.
+
+    """
+    cs_ = np.deg2rad(rectangular_coordinates) if deg_rad == 'deg' else rectangular_coordinates
+
+    x, y, z = cs_
+
+    rho = np.sqrt(x**2 + y**2 + z**2)
+    theta = np.arctan2(y, x)
+    phi = np.arccos(z / rho)
+
+    return rho, theta, phi
